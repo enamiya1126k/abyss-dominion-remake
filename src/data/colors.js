7f1295d3ec6f709b73f2362eaa432b24882ec0a7
enamiya1126k <enamiya1126k@gmail.com>
2026-07-17 00:24:18 +0900

    colors.js を更新

--- src/data/colors.js
+++ src/data/colors.js
@@ -1,1 +1,10 @@
-
+export const MONSTER_COLORS=[
+  {id:"violet",name:"紫",value:"#9b63e6"},
+  {id:"green",name:"緑",value:"#68d27b"},
+  {id:"blue",name:"青",value:"#6bb5ef"},
+  {id:"orange",name:"橙",value:"#e99953"},
+  {id:"red",name:"赤",value:"#df6262"},
+  {id:"pink",name:"桃",value:"#e879b4"},
+  {id:"gold",name:"金",value:"#d9b55c"},
+  {id:"cyan",name:"水",value:"#72d5d8"}
+];
