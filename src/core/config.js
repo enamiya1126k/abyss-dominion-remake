1e3e94e8dc52d9648a0e70de3c99490f3e7ba4d7
enamiya1126k <enamiya1126k@gmail.com>
2026-07-20 22:10:47 +0900

    Add files via upload

--- src/core/config.js
+++ src/core/config.js
@@ -1,4 +1,4 @@
-export const APP_VERSION="0.9.2-alpha.2";
+export const APP_VERSION="0.9.3-alpha.1";
 export const SAVE_KEY="abyss-dominion-remake-v001";
 export const MAX_PARTY_SIZE=4;
 export const MAX_STARS=10;
